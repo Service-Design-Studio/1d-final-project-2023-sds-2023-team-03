5e1d6512a27daa8b215e0efbe151a6634ab94a75
キリエライト <voorheesjason397@gmail.com>
2023-06-14 16:50:51 +0800

    dependencies + test
    added:
    - react-icons
    - echarts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dropdown from './components/dropdown.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div>
+        <Dropdown/>
+      </div>
     </>
   )
 }
