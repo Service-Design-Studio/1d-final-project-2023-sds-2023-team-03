dd32290931b9d5eae9cfb86e0c24c86930fa5301
esannatraj <esannatraj8@gmail.com>
2023-06-15 14:59:28 +0800

    Seeded database with initial product data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product_units_sold = [
+    { name: 'Product A', units_sold: 100 },
+    { name: 'Product B', units_sold: 50 },
+    { name: 'Product C', units_sold: 200 }
+  ]
+  
+  # Seeding data
+  product_units_sold.each do |product_data|
+    product = Product.find_or_initialize_by(name: product_data[:name])
+    product.units_sold = product_data[:units_sold]
+    product.save
+  end
+  
