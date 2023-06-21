87e0225537cfe78ff3a9c3d74d8f98c16e4a9b46
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-06-21 19:31:21 +0800

    added routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import Categories from './views/Categories.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Header from './components/header.jsx'
-import { Stack } from '@mantine/core'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './routes.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Sidebar/>
     <Header/>
-    <Categories/>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
+  </React.StrictMode>
 )
