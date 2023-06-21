067ccdb9a72e2af61b7bef1ea1a9d181209aabd8
benetta-cheng <chengbenetta@gmail.com>
2023-06-21 20:28:14 +0800

    Revert "Added routing for categories to accept query params"
    This reverts commit 93a8db4d186333baf29528d553c94ec0435b7d8c.

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -2,24 +2,17 @@ module Api
   module V1
     class ProductsController < ApplicationController
       def index
-        # Access category, start_date, and end_date parameters from request URL
-        category = params[:category].parameterize if params[:category]
-        start_date = params[:start]
-        end_date = params[:end]
-
         products = Product.all
-        products = products.where("lower(product_category) = ?", category) if category
-        products = products.where(date: start_date..end_date) if start_date && end_date
 
         render json: ProductSerializer.new(products).serialized_json
       end
 
-      # def show
-      #   slugified_product_category = params[:product_category].parameterize
-      #   products = Product.where("lower(product_category) = ?", slugified_product_category)
+      def show
+        slugified_product_category = params[:product_category].parameterize
+        products = Product.where("lower(product_category) = ?", slugified_product_category)
 
-      #   render json: ProductSerializer.new(products).serialized_json
-      # end
+        render json: ProductSerializer.new(products).serialized_json
+      end
     end
   end
 end
