ccb9d0af6edb01ef0785476432d65227c9328367
benetta-cheng <chengbenetta@gmail.com>
2023-06-21 22:14:05 +0800

    Added more seeded data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,13 +8,13 @@
 require 'faker'
 
 # Generate 50 products
-50.times do
+200.times do
   Product.create(
     product_id: Faker::Number.unique.number(digits: 7),
     product_category: ['Comfortwear', 'Running'].sample,
     product_type: ['Shoes', 'Tshirts', 'Shorts', 'Accessories'].sample,
     product_name: Faker::Commerce.product_name,
-    date: Faker::Date.between(from: '2020-01-01', to: Date.today),
+    date: Faker::Date.between(from: '2021-01-01', to: Date.today),
     price: Faker::Commerce.price(range: 10.0..200.0),
     sales: rand(0..200)
   )
