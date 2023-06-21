3275b735df4ad07eefd3ccaaa2dad75fa62f7c51
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-06-22 05:55:09 +0800

    setup for deployment

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Configure 'rails notes' to inspect Cucumber files
   config.annotations.register_directories('features')
   config.annotations.register_extensions('feature') { |tag| /#\s*(#{tag}):?\s*(.*)$/ }
-
+  config.hosts << "sds-team3-backend-v4txkfic3a-as.a.run.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
