52352129cbd214ddb479ade6a920bd538ec2effe
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-07-12 20:17:55 +0800

    refactored queries + added products queries

--- backend/app/controllers/api/v1/sales_controller.rb
+++ backend/app/controllers/api/v1/sales_controller.rb
@@ -4,21 +4,33 @@ module Api
   module V1
     class SalesController < ApplicationController
       def index
+        # set output as empty by default
+        out = {}
+
         # Access category, start_date, and end_date parameters from request URL
         category = params[:category].parameterize if params[:category]
         start_date = params[:start]
         end_date = params[:end]
 
-        sales = Sale.category_time_query(start_date, end_date, category)
-        frequencies = Sale.sales_frequency(sales)
-        revenues = Sale.sales_revenue(sales)
-        types = Sale.top_10_types_category(sales)
-        out = {
-          :frequencies => frequencies,
-          :revenues => revenues,
-          :types => types
-        }
-        render json: out.to_json
+        if params[:category]
+        end
+
+        if params[:start] && params[:end]
+        end
+
+        if params[:category] && params[:start] && params[:end]
+          sales = Sale.category_time_query(start_date, end_date, category)
+          frequencies = Sale.sales_frequency(sales)
+          revenues = Sale.sales_revenue(sales)
+          types = Sale.top_10_types_category(sales)
+          out = {
+            :frequencies => frequencies,
+            :revenues => revenues,
+            :types => types
+          }
+        end
+        
+        render :json => out
       end
     end
   end
