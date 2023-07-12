742188b5f83e859ca3d2fdb8c2dceb1f7a748624
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-07-12 20:17:55 +0800

    refactored queries + added products queries

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ApplicationRecord
+    # Queries for stock < 50
+    def self.products_below_50
+        products = Product.all
+        products = products.where(stock: ..50)
+        products = products.order(stock: :desc)
+    end
 
+    # Searches for ALL products in a category
+    def self.search_category(cat)
+        products = Product.all
+        products = products.where('lower(product_category) = ?', cat)
+    end
+
+    # Filters query to a specific category
+    def self.filter_category(q, cat)
+        products = q.where('lower(product_category) = ?', cat)
+    end
 end
