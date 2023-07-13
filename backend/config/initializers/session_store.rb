b3fa9b59b5674452043f5d8f29a6872a890781d2
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-07-13 12:51:47 +0800

    cookies

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 if Rails.env === 'production' 
     Rails.application.config.session_store :cookie_store, {
         key: '_puma-back-end',
-        domain: 'puma-back-end-json-api',
         same_site: :none,
         secure: true,
     }
