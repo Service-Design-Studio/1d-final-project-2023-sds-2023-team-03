4eef0e94a826d19abce6339d3e0e6b1bc00dafa2
jiasheng-ooi <95675395+jiasheng-ooi@users.noreply.github.com>
2023-07-13 11:33:26 +0800

    change links

--- frontend/features/step-definitions/logout_steps.cjs
+++ frontend/features/step-definitions/logout_steps.cjs
@@ -3,7 +3,7 @@ const { Builder, By, until,actions } = require('selenium-webdriver');
 const assert = require('assert');
 
 const driver = new Builder().forBrowser('chrome').build();
-const websiteUrl = 'http://localhost:5173/';
+const websiteUrl = 'https://sds-team-3-ecommerce-analysis-tool-jvfpcfcafa-as.a.run.app/';
 
 Given('I am on the sidebar', async function () {
   const sidebar = await driver.findElement(By.className('sidebar'));
--- frontend/features/step-definitions/sidebar_steps.cjs
+++ frontend/features/step-definitions/sidebar_steps.cjs
@@ -4,7 +4,7 @@ const { Builder, By, until,actions } = require('selenium-webdriver');
 const assert = require('assert');
 
 const driver = new Builder().forBrowser('chrome').build();
-const websiteUrl = 'http://localhost:5173/';
+const websiteUrl = 'https://sds-team-3-ecommerce-analysis-tool-jvfpcfcafa-as.a.run.app/';
 
 
 // Opening Sidebar (Happy)
