22278cb76d9075630f17adce049cd49ddbf45494
benetta-cheng <chengbenetta@gmail.com>
2023-07-18 15:09:27 +0800

    Created Competitors page

--- frontend/src/views/Competitors.jsx
+++ frontend/src/views/Competitors.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './Competitors.css'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-function Competitors() {
-  return (
-    <div>
-      <h1 id="sales-title">Competitor x </h1>    
+import { Stack, Space } from '@mantine/core'
+import 'tailwindcss/tailwind.css';
+import CarouselCard from '../components/CarouselCard';
+import CompetitorsLineChart from '../components/CompetitorsLineChart'
 
-    </div>
-  );
+const Competitors = () => {
+  const { competitorName } = useParams();
+
+  const [topCompetitorSales, setTopCompetitorSales] = useState([]);
+  // const [allCompetitorData, setAllCompetitorData] = useState([]);
+  const[competitorChartData, setCompetitorChartData] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`http://127.0.0.1:3000/api/v1/competitors/${competitorName}`);
+      // Process the data for the chart
+      const competitorData = result.data.top_sales.map(item => {
+        return {
+          x: new Date(item.date).getTime(), // Convert date to timestamp for chart
+          y: item.sales
+        }
+      });
+
+      setTopCompetitorSales(result.data.top_sales);
+      // setAllCompetitorData(result.data.all_data);
+      setCompetitorChartData(competitorData);
+
+    };
+
+    fetchData();
+  }, [competitorName]);
+
+  return(
+      <>
+          <Stack>
+          <h1 id="competitors-title">{competitorName}'s Analytics</h1>
+          <CarouselCard topProducts={topCompetitorSales} />
+          <CompetitorsLineChart competitorData={competitorChartData} />
+          </Stack>
+      </>
+  )
 }
 
 export default Competitors;
