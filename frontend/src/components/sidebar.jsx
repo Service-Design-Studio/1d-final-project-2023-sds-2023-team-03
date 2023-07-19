f8fa0f80a7c3a38fd50ac347b1c78c27499a6b41
jiasheng-ooi <95675395+jiasheng-ooi@users.noreply.github.com>
2023-07-20 00:45:45 +0800

    top half

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -10,6 +10,7 @@ import Dropdown from './competitor';
 import { Link, useNavigate } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import './Sidebar.css';
+import { RefreshButton } from "@refinedev/mantine";
 import { Modal, Button } from '@mantine/core';
 import { motion } from 'framer-motion';
 import { useSpring, animated,config } from 'react-spring';
