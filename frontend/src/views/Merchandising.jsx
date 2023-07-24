ec85e4f1c098aab67872e415c3cbceff73f00a09
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-07-25 02:58:47 +0800

    "tagging" finished; no data

--- frontend/src/views/Merchandising.jsx
+++ frontend/src/views/Merchandising.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useDisclosure } from '@mantine/hooks'
+import { Modal } from '@mantine/core'
 import './Merchandising.css';
 import MerchandisingTable from '../components/MerchandisingTable.jsx';
 import axios from 'axios';
@@ -6,12 +8,13 @@ import axios from 'axios';
 function Logistics() {
   const [data, setData] = useState([]);
   const [apiLoad, setApiLoad] = useState(true);
+  const [errorOpen, errorModalHandler] = useDisclosure(false);
   const threshold = 50;
   const pageSize = 50;
 
   function getMerchData() {
     setApiLoad(true)
-    axios.get("https://sds-team3-backend-v4txkfic3a-as.a.run.app/api/v1/products/all", {timeout: 10000})
+    axios.get("https://sds-team3-backdend-v4txkfic3a-as.a.run.app/api/v1/products/all", {timeout: 10000})
     .then((res) => {
       if (res && res.data.length > 0) {
         setData(res.data);
@@ -20,6 +23,7 @@ function Logistics() {
     })
     .catch((err) => {
       console.log(err);
+      errorModalHandler.open()
       setApiLoad(false);
     })
   }
@@ -29,10 +33,18 @@ function Logistics() {
   }, [])
   
   return (
-    <div>
+    <>
       <h1 id="sales-title">Merchandising</h1> 
       <MerchandisingTable data={data} threshold={threshold} pageSize={pageSize} apiLoad={apiLoad}/>
-    </div>
+      <Modal
+        centered
+        opened={errorOpen}
+        onClose={errorModalHandler.close}
+        title="Error"
+      > 
+        There was a problem with loading the data. Please retry from the Merchandising page.
+      </Modal>
+    </>
   );
 }
 
