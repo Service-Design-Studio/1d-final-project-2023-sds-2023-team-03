d4262e06502ac83ce858a709d23afcbab4c45d4c
jaedgo12 <jaygohweiwern@gmail.com>
2023-07-24 11:20:49 +0800

    Updated shopee-scraper

--- backend/db/shopee-ruby.rb
+++ backend/db/shopee-ruby.rb
@@ -5,114 +5,265 @@ require 'json'
 require 'cgi'
 
 
+
 USER_AGENTS = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15',
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36 Edg/80.0.361.69',
   # Add more user agents here
 ]
-
-
-Url_dict = {
-  'Running'=> 'https://shopee.sg/mall/search?facet=11012070%2C11012195&keyword=running%20shoes&noCorrection=true&page=0&sortBy=sales',
-  'Running(M)' => 'https://shopee.sg/mall/search?facet=11012070&keyword=running%20shoes&noCorrection=true&page=0&sortBy=sales',
-  'Running(W)' => 'https://shopee.sg/mall/search?facet=11012195&keyword=running%20shoes&noCorrection=true&page=0&sortBy=sales',
-  'Comfortwear' => 'https://shopee.sg/mall/search?complementKeyword=comfortwear%20shoes&facet=11012800%2C11012747&keyword=shoe&originalResultType=4&page=0&sortBy=sales',
-  'Comfortwear(M)' => 'https://shopee.sg/mall/search?complementKeyword=comfortwear%20shoes&facet=11012800&keyword=shoe&originalResultType=4&page=0&sortBy=sales',
-  'Comfortwear(W)' => 'https://shopee.sg/mall/search?complementKeyword=comfortwear%20shoes&facet=11012747&keyword=shoe&originalResultType=4&page=0&sortBy=sales',
-  'Adidas(M)' => 'https://shopee.sg/adidassg?page=0&shopCollection=244592522&sortBy=sales',
-  'Adidas(W)' => 'https://shopee.sg/adidassg?page=0&shopCollection=244349361&sortBy=sales',
-  'Skechers(M)' => 'https://shopee.sg/skecherssg?page=0&shopCollection=244289712&sortBy=sales',
-  'Skechers(W)' => 'https://shopee.sg/skecherssg?page=0&shopCollection=244289273&sortBy=sales',
-  'U_A(M)' => 'https://shopee.sg/under_armour_official?page=0&shopCollection=102499998&sortBy=sales',
-  'U_A(W)' => 'https://shopee.sg/under_armour_official?page=0&shopCollection=102500265&sortBy=sales'
-}
-
-## Class tags dictionary
-
-Name_dict = {
-  'Running' => 'ie3A\\+n\\ bM\\+7UW\\ Cve6sh',
-  'Running(M)' => 'ie3A\\+n\\ bM\\+7UW\\ Cve6sh',
-  'Running(W)' => 'ie3A\\+n\\ bM\\+7UW\\ Cve6sh',
-  'Comfortwear' => 'ie3A\\+n\\ bM\\+7UW\\ Cve6sh',
-  'Comfortwear(M)' => 'ie3A\\+n\\ bM\\+7UW\\ Cve6sh',
-  'Comfortwear(W)' => 'ie3A\\+n\\ bM\\+7UW\\ Cve6sh',
-  'Adidas(M)' => 'h0HBrE\\ ckHqor\\ _5Kkvx1',
-  'Adidas(W)' => 'h0HBrE\\ ckHqor\\ _5Kkvx1',
-  'Skechers(M)' => 'h0HBrE\\ ckHqor\\ _5Kkvx1',
-  'Skechers(W)' => 'h0HBrE\\ ckHqor\\ _5Kkvx1',
-  'U_A(M)' => 'h0HBrE\\ ckHqor\\ _5Kkvx1',
-  'U_A(W)' => 'h0HBrE\\ ckHqor\\ _5Kkvx1'
-              
-}
-
-Price_dict = {
-  'Running' => 'vioxXd\\ rVLWG6',
-  'Running(M)' => 'vioxXd\\ rVLWG6',
-  'Running(W)' => 'vioxXd\\ rVLWG6',
-  'Comfortwear' => 'vioxXd\\ rVLWG6',
-  'Comfortwear(M)' => 'vioxXd\\ rVLWG6',
-  'Comfortwear(W)' => 'vioxXd\\ rVLWG6',
-  'Adidas(M)' => 'zSpiUB\\ Fd5u2V',
-  'Adidas(W)' => 'zSpiUB\\ Fd5u2V',
-  'Skechers(M)' => 'zSpiUB\\ Fd5u2V',
-  'Skechers(W)' => 'zSpiUB\\ Fd5u2V',
-  'U_A(M)' => 'zSpiUB\\ Fd5u2V',
-  'U_A(W)' => 'zSpiUB\\ Fd5u2V'
-}
-
-Sold_dict = {
-  'Running' => 'r6HknA',
-  'Running(M)' => 'r6HknA',
-  'Running(W)' => 'r6HknA',
-  'Comfortwear' => 'r6HknA',
-  'Comfortwear(M)' => 'r6HknA',
-  'Comfortwear(W)' => 'r6HknA',
-  'Adidas(M)' => 'sPnnFI',
-  'Adidas(W)' => 'sPnnFI',
-  'Skechers(M)' => 'sPnnFI',
-  'Skechers(W)' => 'sPnnFI',
-  'U_A(M)' => 'sPnnFI',
-  'U_A(W)' => 'sPnnFI'
-}
-
+page_count = 7
+all_products_urls = []
 
 
 # Classic (GET)
-def send_request
-
-  
+def send_request(all_p_urls,page_count)
 
   # Input search term
 
   ############### CHANGE SEARCH TERM FOR DIFFERENT CATEGORIES ###############
   search_term = 'Running'
   ###########################################################################
+
+  url_dict = {
+    'Running'=> "https://shopee.sg/mall/search?facet=11012070%2C11012195&keyword=running%20shoes&noCorrection=true&page=#{page_count}&sortBy=sales",
+    'Comfortwear' => "https://shopee.sg/mall/search?complementKeyword=comfortwear%20shoes&facet=11012800%2C11012747&keyword=shoe&originalResultType=4&page=#{page_count}&sortBy=sales",
+    'Adidas(M)' => "https://shopee.sg/adidassg?page=#{page_count}&shopCollection=244592522&sortBy=sales",
+    'Adidas(W)' => "https://shopee.sg/adidassg?page=#{page_count}&shopCollection=244349361&sortBy=sales",
+    'Skechers(M)' => "https://shopee.sg/skecherssg?page=#{page_count}&shopCollection=244289712&sortBy=sales",
+    'Skechers(W)' => "https://shopee.sg/skecherssg?page=#{page_count}&shopCollection=244289273&sortBy=sales",
+    'U_A(M)' => "https://shopee.sg/under_armour_official?page=#{page_count}&shopCollection=102499998&sortBy=sales",
+    'U_A(W)' => "https://shopee.sg/under_armour_official?page=#{page_count}&shopCollection=102500265&sortBy=sales"
+  }
+
+  puts "page count is #{page_count+1}"
   
-  puts Url_dict[search_term]
-  url = URI.encode_www_form_component(Url_dict[search_term])
+  puts url_dict[search_term]
+
+  url = URI.encode_www_form_component(url_dict[search_term])
 
   js_scenario = <<~JS
-        {
-          "instructions": [
-          {"scroll_y": 3000},
-          {"wait": 2000},
-          {"scroll_y": 2000},
-          {"wait": 2000},
-          {"scroll_y": 1000},
-          {"wait": 2000},
-          {"scroll_y": 2000},
-          {"wait": 2000},
-          {"scroll_y": 3000},
-          {"wait": 2000},
-          {"scroll_y": 2000},
-          {"wait": 2000},
-          {"scroll_y": 3000}
-          ]}
+  {
+    "instructions": [
+    {"wait": 5000},
+    {"scroll_y": 3000},
+    {"wait": 2300},
+    {"scroll_y": 2250},
+    {"wait": 1700},
+    {"scroll_y": 1000},
+    {"wait": 2000},
+    {"scroll_y": 2000},
+    {"wait": 2053},
+    {"scroll_y": 3000},
+    {"wait": 2735},
+    {"scroll_y": 2000},
+    {"wait": 1956},
+    {"scroll_y": 3000}
+    ]}
   JS
+  
+  # Response_iteration = Response_iteration + 1
 
   uri = URI("https://app.scrapingbee.com/api/v1/?api_key=VNC7VJ04BQLZWL821KJ4ZLG17ON45K4Y56P59QZMDNZBWRFAS0LIK47I3KFH6AMLUXPHIUIFBDOMIOUE&url=#{url}&stealth_proxy=True&wait_browser=load&json_response=True&block_resources=True&block_ads=True&js_scenario=" + CGI.escape(js_scenario))
 
+  # Randomly select a user agent
+  user_agent = USER_AGENTS.sample
+
+  # Create client
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+  # Create Request
+  req = Net::HTTP::Get.new(uri)
+
+  # Set headers to mimic browser behavior
+  req['User-Agent'] = user_agent
+  req['Accept-Language'] = 'en-US,en;q=0.9'
+  req['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+
+  # Fetch Request
+  res = http.request(req)
+  puts "Response HTTP Status Code: #{res.code}"
+  # puts "Response HTTP Response Body: #{res.body}"
+
+  puts '--------------------- Parsing! ---------------------'
+
+
+  # Assuming you have JSON data in bytes format
+  json_data = res.body
+
+  # Decode the bytes data into a string
+  json_string = json_data.force_encoding('utf-8')
+
+  # Parse the JSON string
+  data = JSON.parse(json_string)
+
+  # body_data = data['resolved-url']
+  puts 'resolved-url'
+  puts data['resolved-url']
+
+
+  body_data = data['body']
+
+  # Print body_data keys
+  # puts '----------------------------------'
+  # puts 'BODY'
+  # puts '----------------------------------'
+  # puts body_data
+
+
+  puts '--------------------- Extracting! ---------------------'
+
+
+  # Parse the HTML data
+  soup = Nokogiri::HTML(body_data)
+
+  #Set containers
+  if search_term == 'Comfortwear' || search_term == 'Running'
+    puts 'Scraping by Categories...'
+    soup_container = soup.at_css('div.row\\ shopee-search-item-result__items')
+
+    puts 'getting links, locations and qty sold/mth'
+    link = soup_container.css('a > @href')
+    location = soup_container.css("div.zGGwiV")
+    sold = soup_container.css("div.r6HknA")
+    # img_perlisting = soup_container.css("div.yvbeD6\\ KUUypF")
+    # img = img_perlisting.css("img > @src")
+  else
+    puts 'Scraping by Brands...'
+    soup_container = soup.at_css('div.shop\\-search\\-result\\-view')
+
+    puts 'getting links, locations and qty sold'
+    link = soup_container.css('a > @href')
+    sold = soup_container.css("div.sPnnFI")
+    # img_perlisting = soup_container.css("div.ExVKL4.Gqf95F")
+    # img = img_perlisting.css("img > @src")
+  end
+  
+
+
+  final_link =[]
+
+  link_data = link.map(&:text)
+
+  location_data = Array.new(link_data.length, 'SG')
+
+  sold_data_raw = sold.map(&:text)
+
+  sold_data = []
+
+  sold_data_raw.each do |cleaned|
+    if cleaned.include?('month')
+      sold_data << cleaned.gsub(' sold/month','').to_i
+    elsif cleaned != ''
+      sold_data << cleaned.gsub(' sold','').to_i
+    else
+      sold_data << 0
+    end
+  end
+
+
+  link_data.each do |data|
+    final_link << "https://shopee.sg" + data
+  end
+
+
+  puts "len of urls: #{final_link.length}"
+
+  puts "len of locations: #{location_data.length}"
+
+  puts "len of sold_data: #{sold_data.length}"
+
+  # puts "len of img_urls: #{img_data.length}"
+
+  
+  # zipped_list = final_link.zip(location_data,sold_data,img_data) ## w img
+  zipped_list = final_link.zip(location_data,sold_data)
+  zipped_list.each do |url_location_entry| 
+    all_p_urls << url_location_entry
+  end
+
+  
+  page_bool = soup.at_css("div.shopee-mini-page-controller")
+
+  # puts page_bool
+  if page_bool.to_s.include?("shopee-button-outline shopee-mini-page-controller__next-btn shopee-button-outline--disabled")
+    puts 'next page does not exist, terminating'
+    return
+  else 
+    puts 'moving to next page...'
+  end
+  puts '-------------------------------------------------------------------------------'
+
+  page_count += 1
+
+
+  send_request(all_p_urls,page_count)
+
+rescue StandardError => e
+  puts "HTTP Request failed (#{e.message})"
+end
+
+send_request(all_products_urls,page_count)
+
+puts "len of all listings_data: #{all_products_urls.length}"
+# puts "All urls n loc: #{all_products_urls}"
+
+
+###cleans out non-sg merchants
+all_products_urls.reject! do |url_loc|
+  !url_loc[1].include?('SG') ||
+  !['Shoes', 'shoes', 'shoe', 'Shoe'].any? { |substring| url_loc[0].include?(substring) }
+end
+
+
+puts "len of all listings_data(cleaned): #{all_products_urls.length}"
+
+
+##################################################################
+## ENTERING INTO EACH URL
+##################################################################
+
+puts "............................................"
+puts "............................................"
+puts 'ENTERING INTO URLS'
+puts "............................................"
+puts "............................................"
+
+Brand_dict = {
+  'adidassg' => "Adidas",
+  'asicsofficial'=> "Asics",
+  'skecherssg'=> "Skechers",
+  'skechersglobal.sg'=> "Skechers",
+  'sauconyofficial'=> "Saucony",
+  'under_armour_official'=> "Under Armour",
+  'puma_singapore'=> "Puma",
+  'salomon.sg'=> "Salomon"
+}
+
+Impt_brands = ['adidassg','asicsofficial','skecherssg','sauconyofficial','under_armour_official','puma_singapore','salomon.sg']
+
+final_list = []
+error_urls = []
+
+def send_request_url(final_l,prod_listing,error_urls)
+
+  puts prod_listing[0]
+
+  url = URI.encode_www_form_component(prod_listing[0])
+
+  js_scenario = <<~JS
+  {
+    "instructions": [
+    {"wait": 5386},
+    {"scroll_y": 2544}
+    ]}
+  JS
+  
+  # Response_iteration = Response_iteration + 1
+
+  uri = URI("https://app.scrapingbee.com/api/v1/?api_key=VNC7VJ04BQLZWL821KJ4ZLG17ON45K4Y56P59QZMDNZBWRFAS0LIK47I3KFH6AMLUXPHIUIFBDOMIOUE&url=#{url}&stealth_proxy=True&wait_browser=load&json_response=True&block_resources=True&block_ads=True&js_scenario=" + CGI.escape(js_scenario))
   # Randomly select a user agent
   user_agent = USER_AGENTS.sample
 
@@ -139,8 +290,6 @@ def send_request
 
 
 
-
-
   puts '--------------------- Parsing! ---------------------'
 
 
@@ -153,94 +302,118 @@ def send_request
   # Parse the JSON string
   data = JSON.parse(json_string)
 
-  # Get the list of keys
-  keys = data.keys
-
-  # Print the list of keys
-  puts 'keys:', keys
+  # body_data = data['resolved-url']
+  puts 'resolved-url'
+  puts data['resolved-url']
 
   body_data = data['body']
 
-  # Print body_data keys
-  puts '----------------------------------'
-  puts 'BODY'
-  puts '----------------------------------'
-  puts body_data
-
-
   puts '--------------------- Extracting! ---------------------'
 
-
   # Parse the HTML data
-  soup = Nokogiri::HTML(body_data)
+  soupy = Nokogiri::HTML(body_data)
+  begin
+    soup = soupy.at_css('div.flex-auto.flex-column.swTqJe')
 
-  # Set containers
-  if search_term == 'Running(M)' || search_term == 'Running(W)' || search_term == 'Comfortwear(M)' || search_term == 'Comfortwear(W)' || search_term == 'Comfortwear' || search_term == 'Running'
-    puts 'Scraping by Categories...'
-    soup_container = soup.at_css('div.row\\ shopee-search-item-result__items')
-  else
-    puts 'Scraping by Brands...'
-    soup_container = soup.at_css('div.shop-page_product-list')
-  end
-  puts Name_dict[search_term]
-  puts Sold_dict[search_term]
-  puts Price_dict[search_term]
+    soupz = soupy.at_css('div.oAVg4E')
 
+    competitor_name = soupz.at_css('div.VlDReK')
 
-  # names = soup_container.css("div##{Name_dict[search_term]}")
+    product_name = soup.at_css('div._44qnta')
 
-  # sold = soup_container.css("div##{Sold_dict[search_term]}")
+    product_price = soup.at_css('div.pqTWkA')
 
-  # price = soup_container.css("div##{Price_dict[search_term]}")
-  puts 'soupy!!'
+    img_perlisting = soupy.at_css("div.cxDtMn")
+    product_img = img_perlisting.css("div > @style")
 
-  names = soup_container.css("div.ie3A\\+n\\ bM\\+7UW\\ Cve6sh")
-  sold = soup_container.css("div.r6HknA")
-  price = soup_container.css("div.vioxXd\\ rVLWG6")
+    competitor_name_data = competitor_name.text
 
-  # Extract the data from div elements
+    puts "compet name data: #{competitor_name_data}"
 
-  name_data = names.map(&:text)
-  sold_data_raw = sold.map(&:text)
-  price_data = price.map(&:text)
-
-  sold_data = []
-
-  sold_data_raw.each do |cleaned|
-    if cleaned.include?('month')
-      sold_data << cleaned.gsub(' sold/month','').to_i
-    elsif cleaned != ''
-      sold_data << cleaned.gsub(' sold','').to_i
-    else
-      sold_data << 0
+    unless Impt_brands.include?(competitor_name_data)
+      puts 'Product is not sold by official seller! Skipping...'
+      puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>next product>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+      return
     end
+
+
+    product_name_data = product_name.text
+    product_price_data = product_price.text
+    product_img_data = product_img.to_s
+    product_img_data_f = product_img_data.match(/url\(["']([^"']+)["']\)/)[1]
+
+  rescue NoMethodError => e
+    # Handle the error when 'at_css' method fails due to nil value
+    puts "Skipping URL: #{prod_listing[0]} - Error: #{e.message}"
+
+    # Add the URL that encountered the error to the error_urls list
+    error_urls << prod_listing
+
+    puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>next product>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    return
   end
 
-  puts "len of names: #{name_data.length}"
-  puts "name data: #{name_data}"
 
-  puts "len of units sold: #{sold_data.length}"
-  puts "units sold data: #{sold_data}"
+  puts "Competitor name: #{Brand_dict.fetch(competitor_name_data)}"
 
-  puts "len of prices: #{price_data.length}"
-  puts "prices data: #{price_data}"
-  puts 'KNNBCCB'
+  puts "Product Name: #{product_name_data}"
 
-  fname = name_data.select(&:empty?)
-  fsold = sold_data.select { |value| value.zero? }
-  fprices = price_data.select(&:empty?)
+  puts "Price: #{product_price_data}"
+  
+  puts "Qty sold/month: #{prod_listing[2]} sold/month"
 
-  puts "any empty names?: #{fname.length}"
-  puts "any empty units sold?: #{fsold.length}"
-  puts "any empty prices?: #{fprices.length}"
+  puts "Image Url: #{product_img_data_f}"
+  
+  final_entry = []
+  final_entry << Brand_dict.fetch(competitor_name_data)
+  final_entry << product_name_data
+  final_entry << product_price_data
+  final_entry << prod_listing[2]
+  final_entry << product_img_data_f
 
+  final_l << final_entry
+  puts "final_entry: #{final_entry}"
 
+  puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>next product>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 
 rescue StandardError => e
   puts "HTTP Request failed (#{e.message})"
+  error_urls << prod_listing
 end
 
-send_request()
+all_products_urls.each do |entry|
+  puts "PRODUCT NUMBER: #{all_products_urls.index(entry)+1} out of #{all_products_urls.length}"
+  send_request_url(final_list,entry,error_urls)
+end
+
+puts '---------------------------------------------------------'
+puts '---------------------------------------------------------'
+puts 'Combined Scraped Data'
+puts '---------------------------------------------------------'
+puts '---------------------------------------------------------'
+puts "Length of products: #{final_list.length}"
+puts '---------------------------------------------------------'
 
 
+puts "error_urls: #{error_urls}"
 
+retry_list = error_urls
+error_urls = []
+puts 'retrying error_urls'
+  
+retry_list.each do |entry|
+  puts "RETRYING PRODUCT NUMBER: #{retry_list.index(entry)+1} out of #{retry_list.length}"
+  send_request_url(final_list,entry,error_urls)
+end
+
+puts "final error_urls: #{error_urls}"
+
+count = 0
+
+final_list.each do |product_data|
+  count = count + 1
+  puts "---------------------"
+  puts "Product #{count}:"
+  puts "---------------------"
+  puts product_data
+end
