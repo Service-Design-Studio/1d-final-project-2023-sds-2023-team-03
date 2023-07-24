b4c382671a1aad13239bb5bfbe82851792ebc438
esannatraj <esannatraj8@gmail.com>
2023-07-24 15:08:42 +0800

    Update bigquery_module.rb

--- backend/BQmethods/bigquery_module.rb
+++ backend/BQmethods/bigquery_module.rb
@@ -1,41 +1,72 @@
 # lib/bigquery_module.rb
 
+require "google/cloud/bigquery"
+
 module BigQueryModule
-    require "google/cloud/bigquery"
-  
-    def self.create_dataset
-      project_id = "sds-group3"
-      dataset_id = "ecommerce_data" # Replace with your desired dataset ID
-  
-      bigquery = Google::Cloud::Bigquery.new(project: project_id)
-      dataset = bigquery.create_dataset(dataset_id)
-  
-      if dataset
-        puts "Dataset #{dataset.dataset_id} created."
-      else
-        puts "Dataset creation failed."
-      end
-    end
-  
-    def self.create_table
-      project_id = "sds-group3"
-      dataset_id = "ecommerce_data" # Replace with your dataset ID
-      table_id = "product_sales"    # Replace with your desired table ID
-  
-      bigquery = Google::Cloud::Bigquery.new(project: project_id)
-      dataset = bigquery.dataset(dataset_id)
-  
-      table = dataset.create_table(table_id) do |schema|
-        schema.string "product_name"
-        schema.integer "units_sold"
-        # Add more fields as needed based on your data
-      end
-  
-      if table
-        puts "Table #{table.table_id} created."
-      else
-        puts "Table creation failed."
-      end
+  def self.create_dataset
+    project_id = "sds-group3"
+    dataset_id = "ecommerce_data" # Replace with your desired dataset ID
+
+    bigquery = Google::Cloud::Bigquery.new(project: project_id)
+    dataset = bigquery.create_dataset(dataset_id)
+
+    if dataset
+      puts "Dataset #{dataset.dataset_id} created."
+    else
+      puts "Dataset creation failed."
     end
   end
-  
+
+  def self.create_table
+    project_id = "sds-group3"
+    dataset_id = "ecommerce_data" # Replace with your desired dataset ID
+    table_id = "product_details" # Replace with your desired table ID
+
+    bigquery = Google::Cloud::Bigquery.new(project: project_id)
+    dataset = bigquery.dataset(dataset_id)
+
+    schema = [
+      { name: "brand", type: "STRING" },
+      { name: "product_name", type: "STRING" },
+      { name: "product_price", type: "STRING" },
+      { name: "quantity_sold", type: "INTEGER" },
+      { name: "image_url", type: "STRING" }
+    ]
+
+    table = dataset.create_table(table_id, schema: schema)
+
+    if table
+      puts "Table #{table.table_id} created with schema: #{table.schema.fields}"
+    else
+      puts "Table creation failed."
+    end
+  end
+
+  def self.load_data_into_table(data)
+    project_id = "sds-group3"
+    dataset_id = "ecommerce_data" # Replace with your desired dataset ID
+    table_id = "product_details" # Replace with your desired table ID
+
+    bigquery = Google::Cloud::Bigquery.new(project: project_id)
+    dataset = bigquery.dataset(dataset_id)
+    table = dataset.table(table_id)
+
+    rows = data.map do |entry|
+      {
+        brand: entry[0],
+        product_name: entry[1],
+        product_price: entry[2].to_f,
+        quantity_sold: entry[3].to_i,
+        image_url: entry[4]
+      }
+    end
+
+    response = table.insert(rows)
+
+    if response.success?
+      puts "Data successfully loaded into BigQuery."
+    else
+      puts "Data loading failed: #{response.error}"
+    end
+  end
+end
