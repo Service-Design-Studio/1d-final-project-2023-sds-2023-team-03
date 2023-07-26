763fe5a91172d0da8294c5a10a8c79161b729353
esannatraj <esannatraj8@gmail.com>
2023-07-26 21:58:33 +0800

    testing loading of data with updated seeded data

--- backend/db/bq_seeds.rb
+++ backend/db/bq_seeds.rb
@@ -1,14 +1,10 @@
-# db/seeds.rb
-
-# Seed data
 def final_list
-    [
-      ["Adidas", "Running Shoe A", "99.99", 100, "https://example.com/image1.jpg"],
-      ["Nike", "Running Shoe B", "129.99", 50, "https://example.com/image2.jpg"],
-      ["Puma", "Running Shoe C", "79.99", 75, "https://example.com/image3.jpg"],
-      ["Under Armour", "Running Shoe D", "109.99", 120, "https://example.com/image4.jpg"],
-      ["Skechers", "Running Shoe E", "89.99", 90, "https://example.com/image5.jpg"],
-      ["New Balance", "Running Shoe F", "94.99", 65, "https://example.com/image6.jpg"]
-    ]
-  end
-  
+  [
+    ["Running", "Adidas", "Running Shoe A", "99.99", "89.99", 100, "https://example.com/product1", "https://example.com/image1.jpg", "19/7/2023"],
+    ["Running", "Nike", "Running Shoe B", "129.99", "109.99", 50, "https://example.com/product2", "https://example.com/image2.jpg", "19/7/2023"],
+    ["Running", "Puma", "Running Shoe C", "79.99", "69.99", 75, "https://example.com/product3", "https://example.com/image3.jpg", "19/7/2023"],
+    ["Running", "Under Armour", "Running Shoe D", "109.99", "99.99", 120, "https://example.com/product4", "https://example.com/image4.jpg", "19/7/2023"],
+    ["Running", "Skechers", "Running Shoe E", "89.99", "79.99", 90, "https://example.com/product5", "https://example.com/image5.jpg", "19/7/2023"],
+    ["Running", "New Balance", "Running Shoe F", "94.99", "84.99", 65, "https://example.com/product6", "https://example.com/image6.jpg", "19/7/2023"]
+  ]
+end
--- backend/lib/bigquery_module.rb
+++ backend/lib/bigquery_module.rb
@@ -1,6 +1,8 @@
 # lib/bigquery_module.rb
 
 require "google/cloud/bigquery"
+require 'date'
+#Remember to update project_id, dataset_id, table_id accordingly!!!
 
 module BigQueryModule
   def self.create_dataset
@@ -26,7 +28,7 @@ module BigQueryModule
       { name: "discounted_price", type: "STRING" },
       { name: "quantity_sold", type: "INTEGER" },
       { name: "product_url", type: "STRING" },
-      { name: "image_url", type: "STRING" }
+      { name: "image_url", type: "STRING" },
       { name: "date_api_called", type: "DATE" }
     ]
   end
@@ -63,11 +65,15 @@ module BigQueryModule
 
     rows = data.map do |entry|
       {
-        brand: entry[0],
-        product_name: entry[1],
-        product_price: entry[2],
-        quantity_sold: entry[3].to_i,
-        image_url: entry[4]
+        category: entry[0],
+        brand: entry[1],
+        product_name: entry[2],
+        initial_price: entry[3],
+        discounted_price: entry[4],
+        quantity_sold: entry[5].to_i,
+        product_url:entry[6],
+        image_url: entry[7],
+        date_api_called: Date.parse(entry[8])
       }
     end
 
@@ -78,5 +84,187 @@ module BigQueryModule
     else
       puts "Data loading failed: #{response.error}"
     end
+  end
+
+  def self.fetch_all_products
+    # Configure Google Cloud BigQuery client
+    bigquery = Google::Cloud::Bigquery.new(project: "your_project_id")
+
+    # Specify your dataset and table IDs
+    dataset_id = "your_dataset_id"
+    table_id = "your_table_id"
+
+    # Construct the query to fetch all products
+    query = "SELECT * FROM `#{dataset_id}.#{table_id}`"
+
+    # Execute the query and get the results
+    job = bigquery.query_job(query)
+    results = job.data
+
+    # Process the results and return the data as an array of hashes
+    # Assuming the column names in your table match the keys in the hash
+    products = results.map do |row|
+      {
+        category: row["category"],
+        brand: row["brand"],
+        product_name: row["product_name"],
+        initial_price: row["initial_price"],
+        discounted_price: row["discounted_price"],
+        quantity_sold: row["quantity_sold"],
+        product_url: row["product_url"],
+        image_url: row["image_url"],
+        date_api_called: row["date_api_called"]
+      }
+    end
+
+    products
+  end
+
+  def self.fetch_products_by_category(category)
+    project_id = "sds-group3"
+    dataset_id = "testing_data" # Replace with your desired dataset ID
+    table_id = "test_table" # Replace with your desired table ID
+
+    bigquery = Google::Cloud::Bigquery.new(project: project_id)
+    dataset = bigquery.dataset(dataset_id)
+    table = dataset.table(table_id)
+
+    # Construct the query to fetch products filtered by category
+    query = <<~SQL
+      SELECT *
+      FROM `#{project_id}.#{dataset_id}.#{table_id}`
+      WHERE category = "#{category}"
+    SQL
+
+    job = bigquery.query_job(query)
+    results = job.wait_until_done!
+
+    # Process the results and return an array of hashes representing products filtered by category
+    if results.failed?
+      puts "Error: #{results.error}"
+      return []
+    else
+      products = []
+      results.each do |row|
+        products << {
+          category: row[:category],
+          brand: row[:brand],
+          product_name: row[:product_name],
+          initial_price: row[:initial_price],
+          discounted_price: row[:discounted_price],
+          quantity_sold: row[:quantity_sold],
+          product_url: row[:product_url],
+          image_url: row[:image_url],
+          date_api_called: row[:date_api_called]
+        }
+      end
+      return products
+    end
+  end
+
+  def self.fetch_products_by_brand(brand_name)
+    # Configure Google Cloud BigQuery client
+    bigquery = Google::Cloud::Bigquery.new(project: "your_project_id")
+  
+    # Specify your dataset and table IDs
+    dataset_id = "your_dataset_id"
+    table_id = "your_table_id"
+  
+    # Construct the query to fetch products by brand
+    query = "SELECT * FROM `#{dataset_id}.#{table_id}` WHERE brand = '#{brand_name}'"
+  
+    # Execute the query and get the results
+    job = bigquery.query_job(query)
+    results = job.data
+  
+    # Process the results and return the data as an array of hashes
+    # Assuming the column names in your table match the keys in the hash
+    products = results.map do |row|
+      {
+        category: row["category"],
+        brand: row["brand"],
+        product_name: row["product_name"],
+        initial_price: row["initial_price"],
+        discounted_price: row["discounted_price"],
+        quantity_sold: row["quantity_sold"],
+        product_url: row["product_url"],
+        image_url: row["image_url"],
+        date_api_called: row["date_api_called"]
+      }
+    end
+    products
+  end
+
+  def self.fetch_products_by_date(date)
+    # Configure Google Cloud BigQuery client
+    bigquery = Google::Cloud::Bigquery.new(project: "your_project_id")
+  
+    # Specify your dataset and table IDs
+    dataset_id = "your_dataset_id"
+    table_id = "your_table_id"
+  
+    # Format the date in YYYY-MM-DD format
+    formatted_date = date.strftime("%Y-%m-%d")
+  
+    # Construct the query to fetch products by date
+    query = "SELECT * FROM `#{dataset_id}.#{table_id}` WHERE DATE(date_api_called) = '#{formatted_date}'"
+  
+    # Execute the query and get the results
+    job = bigquery.query_job(query)
+    results = job.data
+  
+    # Process the results and return the data as an array of hashes
+    # Assuming the column names in your table match the keys in the hash
+    products = results.map do |row|
+      {
+        category: row["category"],
+        brand: row["brand"],
+        product_name: row["product_name"],
+        initial_price: row["initial_price"],
+        discounted_price: row["discounted_price"],
+        quantity_sold: row["quantity_sold"],
+        product_url: row["product_url"],
+        image_url: row["image_url"],
+        date_api_called: row["date_api_called"]
+      }
+    end
+    products
+  end
+  
+  def self.sort_by_quantity_sold
+    project_id = "sds-group3"
+    dataset_id = "testing_data" # Replace with your desired dataset ID
+    table_id = "test_table" # Replace with your desired table ID
+
+    bigquery = Google::Cloud::Bigquery.new(project: project_id)
+    dataset = bigquery.dataset(dataset_id)
+    table = dataset.table(table_id)
+
+    # Construct the query to fetch products sorted by quantity_sold in descending order
+    query = <<~SQL
+      SELECT *
+      FROM `#{project_id}.#{dataset_id}.#{table_id}`
+      ORDER BY quantity_sold DESC
+    SQL
+
+    job = bigquery.query_job(query)
+    results = job.wait_until_done!
+
+    # Process the results and return an array of hashes representing products sorted by quantity_sold
+    products = []
+    results.each do |row|
+      products << {
+        category: row[:category],
+        brand: row[:brand],
+        product_name: row[:product_name],
+        initial_price: row[:initial_price],
+        discounted_price: row[:discounted_price],
+        quantity_sold: row[:quantity_sold],
+        product_url: row[:product_url],
+        image_url: row[:image_url],
+        date_api_called: row[:date_api_called]
+      }
+    end
+    products
   end
 end
