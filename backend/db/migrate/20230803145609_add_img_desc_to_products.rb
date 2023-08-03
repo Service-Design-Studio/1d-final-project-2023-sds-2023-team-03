aee9bc90152bf7db0f14cd1b7731d9bf345e5db4
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-08-03 23:13:31 +0800

    added columns, TODO: UPDATE SEEDS

--- backend/db/migrate/20230803145609_add_img_desc_to_products.rb
+++ backend/db/migrate/20230803145609_add_img_desc_to_products.rb
@@ -2,7 +2,5 @@ class AddImgDescToProducts < ActiveRecord::Migration[7.0]
   def change
     add_column :products, :description, :text
     add_column :products, :image_link, :text
-    prods = Product.where('image_link IS NULL and description IS NULL')
-    prods.destroy_all
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_145609) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_150935) do
   create_table "competitors", force: :cascade do |t|
     t.integer "product_id"
     t.string "product_category"
