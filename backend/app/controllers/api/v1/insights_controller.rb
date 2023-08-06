5dfdb1ae336af309f40a2d0276c06165bfde607c
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-08-07 04:30:03 +0800

    updated insights controller

--- backend/app/controllers/api/v1/insights_controller.rb
+++ backend/app/controllers/api/v1/insights_controller.rb
@@ -4,13 +4,41 @@ require './lib/insights/product_insights_driver.rb'
 module Api
     module V1
         class InsightsController < ApplicationController
+            @@available_types = [
+                :products
+            ]
+
+            def available_types
+                @@available_types
+            end
 
             def index
-
+                out = "Available categories: "
+                available_types.each do |type|
+                    if (available_types.find_index(type) == available_types.length-1)
+                        out += "#{type}."
+                    else
+                        out += "#{type},"
+                    end
+                end
+                render :body => out, :json => {:available => available_types.to_json}
             end
 
             def show
-                type = params[:id]
+                if !params.has_key?(:id) 
+                    return
+                end
+                
+                type = params[:id].downcase
+                if (respond_to?(type))
+                    send(type) and return
+                end
+
+                render :body => "Insights for category \"#{type}\" do not exist!", 
+                :status => 404
+            end
+
+            def products
                 sales = Sale.all
                 products = Product.all.as_json
 
