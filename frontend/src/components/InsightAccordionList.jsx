115a6005e1c48b50c4e7be5482431305fea83943
jiasheng-ooi <95675395+jiasheng-ooi@users.noreply.github.com>
2023-08-06 14:33:14 +0800

    icon become big ooo

--- frontend/src/components/InsightAccordionList.jsx
+++ frontend/src/components/InsightAccordionList.jsx
@@ -1,47 +1,72 @@
 import { List, ThemeIcon, HoverCard, Text } from '@mantine/core';
-import { IconCircleCheck, IconCircleDashed,IconArrowBadgeRight } from '@tabler/icons-react';
-import * as React from 'react';
+import { IconInfoCircleFilled, IconInfoCircle, IconArrowBadgeRight } from '@tabler/icons-react';
+import React, { useState } from 'react';
 
 function AccordionList({ listItems }) {
-  return (
-    <div style={{ textAlign: 'left' }}> 
-    <List spacing="xs" size="sm" > {/* Add align="left" here */}
-      {listItems.map((item, index) => (
-        <React.Fragment key={index}>
-          {item.hoverText ? (
-            
-                <List.Item
-                  icon={
-                    <HoverCard width={250} shadow="md">
-                        <HoverCard.Target>
-                            <ThemeIcon color="blue" size={24} radius="xl">
-                            <IconArrowBadgeRight size="1.2rem" />
-                            </ThemeIcon>
-                        </HoverCard.Target>
-                        <HoverCard.Dropdown>
-                            <Text size="s">{item.hoverText}</Text>
-                        </HoverCard.Dropdown>
-                    </HoverCard>
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
-                  }
-                >
-                  {item.text}
-                </List.Item>
-             
-          ) : (
-            <List.Item
-              icon={
-                <ThemeIcon color="teal" size={24} radius="xl">
-                  <IconArrowBadgeRight size="1.2rem" />
-                </ThemeIcon>
-              }
-            >
-              {item.text}
-            </List.Item>
-          )}
-        </React.Fragment>
-      ))}
-    </List>
+  const iconStyles = {
+    icon: {
+      transition: 'all 0.1s ease',
+      transform: 'scale(1.2)',
+      color: 'white',
+    },
+    nonHoveredIcon: {
+      transition: 'all 0.1s ease',
+      transform: 'scale(1)',
+      color: 'white',
+    },
+  };
+
+  return (
+    <div style={{ textAlign: 'left' }}>
+      <List spacing="xs" size="sm" align="left">
+        {listItems.map((item, index) => (
+          <React.Fragment key={index}>
+            {item.hoverText ? (
+              <List.Item
+                
+                icon={
+                   <div onMouseEnter={() => setHoveredIndex(index)}
+                   onMouseLeave={() => setHoveredIndex(null)}>
+                  <HoverCard width={250} shadow="md" >
+                    <HoverCard.Target>
+                      <ThemeIcon
+                        color = 'blue'
+                        size={24}
+                        radius="xl"
+                        style={
+                          hoveredIndex === index
+                            ? iconStyles.icon
+                            : iconStyles.nonHoveredIcon
+                        }
+                      >
+                        <IconInfoCircle size="1.2rem" />
+                      </ThemeIcon>
+                    </HoverCard.Target>
+                    <HoverCard.Dropdown>
+                      <Text size="s">{item.hoverText}</Text>
+                    </HoverCard.Dropdown>
+                  </HoverCard>
+                  </div>
+                }
+              >
+                {item.text}
+              </List.Item>
+            ) : (
+              <List.Item
+                icon={
+                  <ThemeIcon color="teal" size={24} radius="xl">
+                    <IconInfoCircle size="1.2rem" />
+                  </ThemeIcon>
+                }
+              >
+                {item.text}
+              </List.Item>
+            )}
+          </React.Fragment>
+        ))}
+      </List>
     </div>
   );
 }
