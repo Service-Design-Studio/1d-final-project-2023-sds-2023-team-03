97a3843d3a4cd9a0e320e24479140dde77d0d3d4
dl3on <124175480+dl3on@users.noreply.github.com>
2023-08-07 17:32:03 +0800

    Merch page insights complete w/o AI suggestions

--- frontend/src/components/merchandising_components/ProductInsights.jsx
+++ frontend/src/components/merchandising_components/ProductInsights.jsx
@@ -1,5 +1,18 @@
 import { useState, useEffect } from 'react'
-import { Badge, Group, Stack } from '@mantine/core'
+import { Badge, Group, Stack, Text } from '@mantine/core'
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+    container: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      gap: 5,
+      paddingLeft: '15px',
+    },
+    badge: {
+        flexShrink: 0,
+      },
+  }));
 
 function ProductInsights({ insightData }) {
     const [data, setData] = useState([])
@@ -11,20 +24,28 @@ function ProductInsights({ insightData }) {
             .sort((a, b) => {
                 var keyA = a.severity.level;
                 var keyB = b.severity.level;
-                if (keyA > keyB) return 1;
-                if (keyA < keyB) return -1;
+                if (keyA > keyB) return -1;
+                if (keyA < keyB) return 1;
                 return 0;
             }))
 
     }, [insightData])
-
+    const classes = useStyles();
     return (
         <Stack spacing = "0">{data.map((ins) => {
+            let textColor = ins.severity.label === 'Moderate' ? 'dark' : (ins.severity.label === 'Critical' || ins.severity.label === 'Severe' ? 'red' : 'green');
             return (
-                <Group spacing = "xs">
-                    <Badge>{ins.severity.label}</Badge>
-                    <p>{ins.text}</p>
-                </Group>
+                <div className={classes.container}>
+                    <Badge className={classes.badge}
+                     color={textColor}
+                     variant = {ins.severity.label === 'Critical' || ins.severity.label === 'Exceptional' ? 'filled' : 'outline'}
+                     >
+                        {ins.severity.label}
+                    </Badge>
+                    <Text style={{ color: textColor }}>
+                        {ins.text}
+                    </Text>
+                </div>
             )
         })}</Stack>
     )
