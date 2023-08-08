8bde619175c516e13282defda55594586c3b67cc
jiasheng-ooi <95675395+jiasheng-ooi@users.noreply.github.com>
2023-08-08 11:10:05 +0800

    edit typo

--- frontend/src/components/InsightAccordions.jsx
+++ frontend/src/components/InsightAccordions.jsx
@@ -9,7 +9,7 @@ function InsightAccordions({ categorizedInsights }) {
   return (
     <div style={{ paddingTop: '3em', paddingBottom: '2em' }}>
       <Accordion variant="separated" chevronPosition="left" defaultValue="customization">
-        {categorizedInsights.Critical_Insight.length  && (
+        {categorizedInsights.Critical_Insight.length>0  && (
           <Accordion.Item value="Critical">
             <Accordion.Control>
               <Group noWrap>
