06ea621ba721561bb0639b455b550229eb355fb9
horouuu <fernanda_fadja@mymail.sutd.edu.sg>
2023-08-10 03:28:11 +0800

    added seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -48,6 +48,23 @@ existing_product_ids = Product.all.pluck(:product_id)
   )
 end
 
+sales = Sale.all
+products = Product.all
+
+products.each do |product|
+  product_sales = sales.where('product_id = ?', product.product_id)
+  if (!product_sales)
+    next
+  end
+
+  qty_sold = 0
+  product_sales.each do |sale|
+    qty_sold += sale.sales
+  end
+
+  product.update(units_sold: qty_sold)
+end
+
 # Generate 200 competitors
 200.times do
   Competitor.create(
